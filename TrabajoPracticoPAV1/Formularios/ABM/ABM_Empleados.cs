b528429764f8529f1f4dff5aa760ec17968344ad
MatiasPerlo83723 <matias.perlo.arg@gmail.com>
2021-05-05 15:55:18 -0300

    Cambios nuevos

---
+++ TrabajoPracticoPAV1/Formularios/ABM/ABM_Empleados.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TrabajoPracticoPAV1.Formularios.ABM
+{
+    public partial class ABM_Empleados : Form
+    {
+        public ABM_Empleados()
+        {
+            InitializeComponent();
+        }
+    }
+}
