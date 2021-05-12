a02f45549068283b08660994cc3a920e6d9ee897
Fer <fer.j.noceti@gmail.com>
2021-05-12 11:46:32 -0300

    abm_laboratorios

---
+++ TrabajoPracticoPAV1/Formularios/ABM/ABM_Laboratorios.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TrabajoPracticoPAV1.Formularios.ABM
+{
+    public partial class ABM_Laboratorios : Form
+    {
+        public ABM_Laboratorios()
+        {
+            InitializeComponent();
+        }
+    }
+}
